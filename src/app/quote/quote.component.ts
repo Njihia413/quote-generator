1c3b606493e852b8f1c2dff8e26ca9c9331f2d73
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-21 18:08:19 +0300

    Nest quote details using quote detail selector

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, statement:'A website without visitors is like a ship lost in the horizon', author:'Dr. Christopher Dayagdag', createdBy:'Maureen'},
-    {id:2, statement:'A user interface is like a joke. If you have to explain it, it is not that good', author:'Unknown', createdBy:'Maureen'},
-    {id:3, statement:'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away', author:'Antonie De-Saint Exupery', createdBy:'Maureen'},
-    {id:4, statement:'Great web design without functionality is like a sports car with no engine', author:'Paul Cookson', createdBy:'Maureen'},
-    {id:5, statement:'It is not a bug, it is an undocumented feature!', author:'Anonymous', createdBy:'Maureen'},
+    new Quote(1,'A website without visitors is like a ship lost in the horizon','Dr. Christopher Dayagdag','Maureen'),
+    new Quote(2,'A user interface is like a joke. If you have to explain it, it is not that good','Unknown', 'Maureen'),
+    new Quote(3,'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away','Antonie De-Saint Exupery','Maureen'),
+    new Quote(4,'Great web design without functionality is like a sports car with no engine','Paul Cookson','Maureen'),
+    new Quote(5,'It is not a bug, it is an undocumented feature!','Anonymous','Maureen'),
   ]
 
   constructor() { }
