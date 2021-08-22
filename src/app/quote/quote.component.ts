6da3a3ae17b794758628df569fe3a226df4996c0
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-22 22:49:29 +0300

    Create form, style form and add form logic

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,40 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1,'A website without visitors is like a ship lost in the horizon','Dr. Christopher Dayagdag','Maureen'),
-    new Quote(2,'A user interface is like a joke. If you have to explain it, it is not that good','Unknown', 'Maureen'),
-    new Quote(3,'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away','Antonie De-Saint Exupery','Maureen'),
-    new Quote(4,'Great web design without functionality is like a sports car with no engine','Paul Cookson','Maureen'),
-    new Quote(5,'It is not a bug, it is an undocumented feature!','Anonymous','Maureen'),
+    new Quote(
+    1,
+    'Dr. Christopher Dayagdag',
+    'Atara Njihia',
+    'A website without visitors is like a ship lost in the horizon',
+    new Date(2021,8,22)),
+    
+    new Quote(
+    2,
+    'Unknown',
+    'Magdalene Mwaniki',
+    'A user interface is like a joke. If you have to explain it, it is not that good', 
+    new Date(2021,8,22)),
+
+    new Quote(
+    3,
+    'Antonie De-Saint Exupery',
+    'Lyn Muthoni',
+    'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away',
+    new Date(2021,8,22)),
+
+    new Quote(
+    4,
+    'Paul Cookson',
+    'Jonah Kanyiri',
+    'Great web design without functionality is like a sports car with no engine',
+    new Date(2021,8,22)),
+
+    new Quote(
+    5,
+    'Anonymous',
+    'Maureen Njihia',
+    'It is not a bug, it is an undocumented feature!',
+    new Date(2021,8,22)),
   ]
   goals: any;
 
