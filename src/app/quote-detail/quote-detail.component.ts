5f88f96a9d8e255ae6d9e591291f6e9708c892cc
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-22 11:59:57 +0300

    Add click event binding syntax

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -10,6 +10,29 @@ export class QuoteDetailComponent implements OnInit {
 
   //input property binding
   @Input()quote!: Quote;
+
+  //event emitters
+  //delete event emitter
+  @Output() deleteQuoteEvent = new EventEmitter<Quote>();
+
+  //upvote event emitter
+  @Output() upVoteQuoteEvent = new EventEmitter<Quote>();
+
+  //downvote event emitter
+  @Output() downVoteQuoteEvent = new EventEmitter<Quote>();
+
+
+  deleteQuote(quote:Quote) {
+    this.deleteQuoteEvent.emit(quote);
+  }
+
+  upVoteQuote(quote:Quote) {
+    this.upVoteQuoteEvent.emit(quote);
+  }
+
+  downVoteQuote(quote:Quote) {
+    this.downVoteQuoteEvent.emit(quote);
+  }
   constructor() { }
 
   ngOnInit(): void {
